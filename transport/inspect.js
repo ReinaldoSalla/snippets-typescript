9b6922031bca81a752a567c3d782099678ddbb1b
Reinaldo Salla <reinaldosalla95@gmail.com>
2020-06-11 17:46:34 -0300

    First commit

---
+++ transport/inspect.js
@@ -0,0 +1,9 @@
+//https://developers.google.com/web/tools/chrome-devtools/console/api#table
+var names = ["js", , "node", "react"];
+console.table(names);
+var topics = {
+    progrLang: "JavaScript",
+    backend: "Node",
+    frontend: "React"
+};
+console.table(topics);
---
+++ transport/inspect.ts
@@ -0,0 +1,13 @@
+//https://developers.google.com/web/tools/chrome-devtools/console/api#table
+
+const names = ["js",, "node", "react"];
+
+console.table(names);
+
+const topics = {
+	progrLang: "JavaScript",
+	backend: "Node",
+	frontend: "React"
+};
+
+console.table(topics);
