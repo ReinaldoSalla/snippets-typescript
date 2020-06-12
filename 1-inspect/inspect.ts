ce36dbc1f6d09efc00603974d80b88bc2a78eb53
Reinaldo Salla <reinaldo.salla@compasso.com.br>
2020-06-12 17:22:09 -0300

    Stated to test nested objs

--- 1-inspect/inspect.ts
+++ 1-inspect/inspect.ts
@@ -1,5 +1,17 @@
 /* npx ts-node inspect.ts*/
 
+interface Primitive {
+	[key: string]: boolean | number | string
+}
+
+const processPrimitive = (obj: Primitive): void => {
+	const varName = Object.keys(obj)[0];
+	const varValue = Object.values(obj)[0];
+	console.log(`\n${"*".repeat(100)}`);
+	console.log(`variable "${varName}" is a primitive of type ${typeof varName}`);
+	console.log(varValue);
+};
+
 const inspect = (obj: object): void => {
 	Object.entries(obj).forEach(([key, value]) => {
 		if (Array.isArray(value)) {
@@ -11,13 +23,16 @@ const inspect = (obj: object): void => {
 			console.log(`variable "${key} is an object`);
 			console.table(value);
 		} else {
-			console.log(`\n${"*".repeat(100)}`);
-			console.log(
-				`variable "${key}" is a primitive of type ${typeof value}`
-			);
-			console.log(value);
+			// how can I send the varName and the varValue
+			const test = {
+				key: key,
+				value: value
+			};
+			console.log(test);
+			//processPrimitive({key, value});
 		}
 	});
+	console.log("\nEND\n")
 };
 
 const progrLang = "JavaScript";
@@ -34,13 +49,27 @@ const sequence = [
 	isExpert
 ];
 
-const topics = { 
+const currentTopics = { 
 	progrLang, 
 	backend, 
 	frontend, 
 	yearsOfExperience,
 	isExpert 
 };
+
+const futureTopics = {
+	currentTopics,
+	futureTopics: {
+		mobile: "React-Native",
+		graphs: "WebGL, Threejs, BabylonJS, react-three-fiber"
+	}
+};
+
+console.log(Object.keys(futureTopics).length);
+
+Object.entries(futureTopics).forEach(([key, value]) => {
+	console.table(value);
+});
 
 inspect({
 	progrLang,
@@ -49,6 +78,5 @@ inspect({
 	yearsOfExperience,
 	isExpert,
 	sequence,
-	topics
+	currentTopics
 });
-
